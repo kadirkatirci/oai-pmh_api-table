9ff06c81b0b45536cd44564d53ccc87324310f7f
Kadir KATIRCI <katircikadir@gmail.com>
2023-08-08 10:03:33 +0300

    example grid

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+const grid = new Grid({
+    sort: true,
+    search: true,
+    pagination: true,
+    columns: ['Location', 'Change Frequency', 'Priority'],
+    server: {
+      url: 'https://gridjs.io/sitemap.xml',
+      handle: (res) => {
+        return res.text().then(str => (new window.DOMParser()).parseFromString(str, "text/xml"));
+      },
+      then: data => {
+        return Array.from(data.querySelectorAll('url'))
+          .map(row => [
+            row.querySelector('loc').innerHTML,
+            row.querySelector('changefreq').innerHTML,
+            row.querySelector('priority').innerHTML,
+          ]);
+      }
+    }
+  });
