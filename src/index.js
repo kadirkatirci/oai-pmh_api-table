f4c0f54d11f45f8876ad8545a3529a707f6ba627
Kadir KATIRCI <katircikadir@gmail.com>
2023-08-09 02:55:30 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ new gridjs.Grid({
     pagination: true,
     columns: ["Location", "Change Frequency", "Priority"],
     server: {
-        url: 'https://kadirkatirci.github.io/oai-pmh_api-table/src/sitemap.xml',
+        url: 'https://kadirkatirci.github.io/oai-pmh_api-table/src/api.xml',
         handle: (res) => {
           return res.text().then(str => (new window.DOMParser()).parseFromString(str, "text/xml"));
         },
         then: data => {
-          return Array.from(data.querySelectorAll('url'))
+          return Array.from(data.querySelectorAll('oai_dc'))
             .map(row => [
-              row.querySelector('loc').innerHTML,
-              row.querySelector('changefreq').innerHTML,
-              row.querySelector('priority').innerHTML,
+              row.querySelector('type').innerHTML,
+              row.querySelector('format').innerHTML,
+              row.querySelector('language').innerHTML,
             ]);
       }
     }
