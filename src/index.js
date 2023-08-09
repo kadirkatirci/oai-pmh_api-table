83e0c2070ebc6fa8dd3daf8634ed1732e23cbb25
Kadir KATIRCI <katircikadir@gmail.com>
2023-08-09 03:57:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ new gridjs.Grid({
           return res.text().then(str => (new window.DOMParser()).parseFromString(str, "text/xml"));
         },
         then: data => {
-          return Array.from(data.querySelectorAll(krkt))
+          return Array.from(data.querySelectorAll('[oai_dc="dc"]'))
             .map(row => [
               row.querySelector('type').innerHTML,
               row.querySelector('format').innerHTML,
